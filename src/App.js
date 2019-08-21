c8d1ef18e0e253aee79cd4cae10753d88c6ed8f4
Luis Montes <monteslu@gmail.com>
2019-08-20 17:04:54 -0700

    working j5

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+import five, { Board } from 'johnny-five';
+import { Firmata } from 'firmata-io';
+import SerialPort from 'avrgirl-arduino/lib/browser-serialport';
+import hrtime from 'browser-process-hrtime';
+
 import './App.css';
+
+process.hrtime = hrtime;
+global.five = five;
+
+function handleStart() {
+  const io = global.io = new Firmata(new SerialPort({}));
+  io.once('ready', () => {
+    const board = global.board =  new Board({io, repl: false});  
+    board.on('ready', () => {
+      console.log('johnny five in browser !!!!');
+      const led = new five.Led(13);
+      // "blink" the led in 500ms on-off phase periods
+      led.blink(500);
+    });
+    board.on('error', console.error);
+  });
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Look in dev tools.  You should have <span style={{color: 'yellow'}}>five, board, & io</span> objects.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={handleStart}>start</button>
       </header>
     </div>
   );
